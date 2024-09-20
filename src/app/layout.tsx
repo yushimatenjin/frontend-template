71fdf12fb7a0d4984044addc3305ab2746ac9f5a
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2024-09-20 09:56:34 +0900

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+import "./globals.css";
+
+const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "テンプレート",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={notoSansJP.className}>{children}</body>
+    </html>
+  );
+}
